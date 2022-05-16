802073a1dc58d549c665c7defde8d33d9dd6d4b6
mohammadrezababaeimosleh <mohammadrezababaeimosleh@gmail.com>
2022-05-15 21:08:28 -1000

    crearting ingradiant.h

--- include/ingredient.h
+++ include/ingredient.h
@@ -1,4 +1,28 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
 
+class Ingredient
+{
+public:
+    double get_price_unit(){return price_unit;};
+    size_t get_units(){return price_unit;};
+    virtual std::string get_name() = 0
+    {
+        return name;
+    };
+
+    double price()
+    {
+        return price_unit * units;
+    }
+    
+
+protected:
+    Ingredient(double price_unit, size_t units);
+
+    double price_unit;
+    size_t units;
+    std::string name;
+};
+
 #endif // INGREDIENT_H
